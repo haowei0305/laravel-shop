846055a53c884c224c6e9811b8c34ce836e050f3
haowei0305 <917361785@qq.com>
2020-02-28 16:33:32 +0800

    修改代码

--- src/Wap/Member/Providers/MemberServiceProvider.php
+++ src/Wap/Member/Providers/MemberServiceProvider.php
@@ -6,7 +6,7 @@
  * Time: 15:47
  */
 namespace Linhaowei\LaravelShop\Wap\Member\Providers;
-
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class MemberServiceProvider extends ServiceProvider{
@@ -16,6 +16,21 @@ class MemberServiceProvider extends ServiceProvider{
 
 
     public function registerRoutes(){
-        $this->loadRoutesFrom(__DIR__ . '/../Http/routes.php');
+        Route::group($this->routeConfiguration(), function () {
+            $this->loadRoutesFrom(__DIR__ . '/../Http/routes.php');
+        });
+    }
+
+    public function routeConfiguration(){
+        return [
+            // 定义访问路由的域名
+            // 'domain' => config('telescope.domain', null),
+            // 是定义路由的命名空间
+            'namespace' => 'Linhaowei\LaravelShop\Wap\Member\Http\Controllers',
+            // 这是前缀
+            'prefix' => 'wap/member',
+            //引入web中间件,预防session错误不断刷新出现
+            'middleware' => 'web',
+        ];
     }
 }
