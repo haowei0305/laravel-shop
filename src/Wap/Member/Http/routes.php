322e9c6c1a580f8a1a9b51cb8a50008a4a8c6a9b
haowei0305 <917361785@qq.com>
2020-02-28 15:59:15 +0800

    'src';

---
+++ src/Wap/Member/Http/routes.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2020/2/28
+ * Time: 15:26
+ */
+Route::get('/wetch/wap', 'MemberController@wap');
---
+++ src/Wap/Member/Providers/MemberServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2020/2/28
+ * Time: 15:47
+ */
+namespace Linhaowei\LaravelShop\Wap\Member\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class MemberServiceProvider extends ServiceProvider{
+    public function register(){
+        $this->registerRoutes();
+    }
+
+
+    public function registerRoutes(){
+        $this->loadRoutesFrom(__DIR__ . '/../Http/routes.php');
+    }
+}
