d541d085445b7575409f98e060f22552b120823f
haowei0305 <917361785@qq.com>
2020-02-28 16:03:25 +0800

    修改代码

--- src/Wap/Member/Http/Controllers/MemberController.php
+++ src/Wap/Member/Http/Controllers/MemberController.php
@@ -5,3 +5,9 @@
  * Date: 2020/2/28
  * Time: 15:36
  */
+namespace Linhaowei\LaravelShop\Wap\Member\Http\Controllers;
+use Illuminate\Routing\Controller;
+
+class MemberController extends  Controller{
+
+}
