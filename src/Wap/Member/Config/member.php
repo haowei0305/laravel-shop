774a6922f91b129fb4d05a8018d376391c8a6872
haowei0305 <917361785@qq.com>
2020-03-01 21:46:10 +0800

    laravel-shop

--- src/Wap/Member/Config/member.php
+++ src/Wap/Member/Config/member.php
@@ -2,71 +2,58 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Authentication Defaults
-    |--------------------------------------------------------------------------
-    |
-    | This option controls the default authentication "guard" and password
-    | reset options for your application. You may change these defaults
-    | as required, but they're a perfect start for most applications.
-    |
-    */
+    'auth' => [
+        'controller' => \Linhaowei\LaravelShop\Wap\Member\Http\Controllers\MemberController::class,
+        'guard' => 'member',
 
-    'defaults' => [
-        'guard' => 'web',
-        'passwords' => 'users',
-    ],
+        /*
+        |--------------------------------------------------------------------------
+        | Authentication Guards
+        |--------------------------------------------------------------------------
+        |
+        | Next, you may define every authentication guard for your application.
+        | Of course, a great default configuration has been defined for you
+        | here which uses session storage and the Eloquent user provider.
+        |
+        | All authentication drivers have a user provider. This defines how the
+        | users are actually retrieved out of your database or other storage
+        | mechanisms used by this application to persist your user's data.
+        |
+        | Supported: "session", "token"
+        |
+        */
 
-    /*
-    |--------------------------------------------------------------------------
-    | Authentication Guards
-    |--------------------------------------------------------------------------
-    |
-    | Next, you may define every authentication guard for your application.
-    | Of course, a great default configuration has been defined for you
-    | here which uses session storage and the Eloquent user provider.
-    |
-    | All authentication drivers have a user provider. This defines how the
-    | users are actually retrieved out of your database or other storage
-    | mechanisms used by this application to persist your user's data.
-    |
-    | Supported: "session", "token"
-    |
-    */
-
-    'guards' => [
-        'member' => [
-            'driver' => 'session',
-            'provider' => 'member',
+        'guards' => [
+            'member' => [
+                'driver' => 'session',
+                'provider' => 'member',
+            ],
         ],
-    ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | User Providers
-    |--------------------------------------------------------------------------
-    |
-    | All authentication drivers have a user provider. This defines how the
-    | users are actually retrieved out of your database or other storage
-    | mechanisms used by this application to persist your user's data.
-    |
-    | If you have multiple user tables or models you may configure multiple
-    | sources which represent each model / table. These sources may then
-    | be assigned to any extra authentication guards you have defined.
-    |
-    | Supported: "database", "eloquent"
-    |
-    */
+        /*
+        |--------------------------------------------------------------------------
+        | User Providers
+        |--------------------------------------------------------------------------
+        |
+        | All authentication drivers have a user provider. This defines how the
+        | users are actually retrieved out of your database or other storage
+        | mechanisms used by this application to persist your user's data.
+        |
+        | If you have multiple user tables or models you may configure multiple
+        | sources which represent each model / table. These sources may then
+        | be assigned to any extra authentication guards you have defined.
+        |
+        | Supported: "database", "eloquent"
+        |
+        */
 
-    'providers' => [
-        'member' => [
-            'driver' => 'eloquent',
-            'model' => \Linhaowei\LaravelShop\Wap\Member\Model\User::class,
+        'providers' => [
+            'member' => [
+                'driver' => 'eloquent',
+                'model' => \Linhaowei\LaravelShop\Wap\Member\Model\User::class,
 
+            ],
         ],
-    ],
 
 
-
-];
+    ]];
